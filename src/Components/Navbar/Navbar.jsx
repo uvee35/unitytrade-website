a66133fad1c327dcad0899106096d979e8a201cd
Z.Sabori <zsabori82@yahoo.com>
2024-03-12 20:59:09 +0000

    added starter code

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+const Home = () => <div>Home</div>;
+const Products = () => <div>Products</div>;
+const Cart = () => <div>Cart</div>;
+const Contact = () => <div>Contact</div>;
+
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/products" component={Products} />
+      <Route path="/cart" component={Cart} />
+      <Route path="/contact" component={Contact} />
+    </Router>
+  );
+};
+
+export default App;
