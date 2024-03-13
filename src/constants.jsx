0d90313eac5c5ef805075cfdc9d8d81c9890897b
unknown <bardhoshieugen2@gmail.com>
2024-03-13 20:55:31 +0000

    changed and added carousel

---
+++ src/constants.jsx
@@ -0,0 +1,51 @@
+import Technology from "../src/assets/Images/technology.jpg";
+
+import {
+    RxCrop,
+    RxDesktop,
+    RxPencil2,
+    RxReader,
+    RxRocket,
+    RxAccessibility,
+  } from "react-icons/rx";
+  
+  
+  
+  export const ServiceData = [
+    {
+      icon: RxCrop,
+      title: "Development",
+      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+      backgroundImage: Technology,
+    },
+    {
+      icon: RxPencil2,
+      title: "Branding",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      
+    },
+    {
+      icon: RxDesktop,
+      title: "Design",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      
+    },
+    {
+      icon: RxReader,
+      title: "Seo",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      
+    },
+    {
+      icon: RxAccessibility,
+      title: "Management",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      
+    },
+    {
+      icon: RxRocket,
+      title: "Production",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      
+    },
+  ];
