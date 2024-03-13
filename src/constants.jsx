502d04d5e29e783581951acf78b1e13204afbd22
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-13 21:47:10 +0000

    Update ServiceData in constants.jsx

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,51 +1,42 @@
 import Technology from "../src/assets/Images/technology.jpg";
+import Jewellery from "../src/assets/Images/jewellery.jpg";
 
 import {
-    RxCrop,
-    RxDesktop,
-    RxPencil2,
-    RxReader,
-    RxRocket,
-    RxAccessibility,
-  } from "react-icons/rx";
-  
-  
-  
-  export const ServiceData = [
-    {
-      icon: RxCrop,
-      title: "Development",
-      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
-      backgroundImage: Technology,
-    },
-    {
-      icon: RxPencil2,
-      title: "Branding",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      
-    },
-    {
-      icon: RxDesktop,
-      title: "Design",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      
-    },
-    {
-      icon: RxReader,
-      title: "Seo",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      
-    },
-    {
-      icon: RxAccessibility,
-      title: "Management",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      
-    },
-    {
-      icon: RxRocket,
-      title: "Production",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      
-    },
-  ];
+  RxCrop,
+  RxDesktop,
+  RxPencil2,
+  RxReader,
+  RxRocket,
+  RxAccessibility,
+} from "react-icons/rx";
+
+export const ServiceData = [
+  {
+    icon: RxRocket,
+    title: "Jewellery",
+
+    backgroundImage: Jewellery,
+  },
+  {
+    icon: RxPencil2,
+    title: "Branding",
+  },
+  {
+    icon: RxDesktop,
+    title: "Technology",
+
+    backgroundImage: Technology,
+  },
+  {
+    icon: RxReader,
+    title: "Seo",
+  },
+  {
+    icon: RxAccessibility,
+    title: "Management",
+  },
+  {
+    icon: RxRocket,
+    title: "Production",
+  },
+];
