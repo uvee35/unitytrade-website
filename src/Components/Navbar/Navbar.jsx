3edb09e2b35b2a02c3ee55d1132b7660bb12e04a
Z.Sabori <zsabori82@yahoo.com>
2024-03-13 22:56:16 +0000

    changes in navbar, added basic css code

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,33 @@
-// src/Navbar.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import NavbarBootstrap from 'react-bootstrap/Navbar'; // Changed the name here
-import './Navbar.css';
+import NavbarBootstrap from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import "./Navbar.css";
 
-export default function CustomNavbar() { // Changed the name here
+export default function Navbar() {
+  useEffect(() => {
+    console.log('Navbar component mounted');
+    return () => {
+      console.log('Navbar component will unmount');
+    };
+  }, []);
+
   return (
-    <NavbarBootstrap expand="lg" className="bg-body-tertiary">
+    <Navbar bg="dark" data-bs-theme="dark">
       <Container>
         <NavbarBootstrap.Brand href="#home">Corporate</NavbarBootstrap.Brand>
         <NavbarBootstrap.Toggle aria-controls="basic-navbar-nav" />
         <NavbarBootstrap.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#categories">Categories</Nav.Link> {/* Corrected the spelling of "Categories" */}
+            <Nav.Link href="#categories">Categories</Nav.Link>
             <Nav.Link href="#sign-in">Sign in</Nav.Link>
             <Nav.Link href="#register">Register</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link> {/* Corrected the spelling of "Contact" */}
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </NavbarBootstrap.Collapse>
       </Container>
-    </NavbarBootstrap>
+    </Navbar>
   );
 }
