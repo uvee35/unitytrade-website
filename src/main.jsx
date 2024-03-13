6cdea7010afc1849d15a91c6b14d9598f4288f8a
harrynewt0n <harry-newton@outlook.com>
2024-03-13 20:25:30 +0000

    added bootstrap CSS to Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD:src/main.tsx
-import App from './App.tsx';
-import'bootstrap/dist/css/bootstrap.min.css';
-import * as bootstrap from 'bootstrap';
-import './index.css';
-=======
 import App from './App.jsx'
 import './index.css'
->>>>>>> main:src/main.jsx
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
