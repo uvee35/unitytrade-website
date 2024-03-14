3fba685de5b7d6b8ab1f56022654f0b56984cc14
Z.Sabori <zsabori82@yahoo.com>
2024-03-14 05:34:26 +0000

    changes in navbar and css file

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,30 @@
-import React, { useEffect } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavbarBootstrap from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React, { useState } from 'react';
 import "./Navbar.css";
+import logo from "./logo.png";
+import cartIcon from "./cart_icon.png";
 
-export default function Navbar() {
-  useEffect(() => {
-    console.log('Navbar component mounted');
-    return () => {
-      console.log('Navbar component will unmount');
-    };
-  }, []);
-
+const Navbar=() =>{
+  const[menu,setMenu] =useState("shop")
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
-      <Container>
-        <NavbarBootstrap.Brand href="#home">Corporate</NavbarBootstrap.Brand>
-        <NavbarBootstrap.Toggle aria-controls="basic-navbar-nav" />
-        <NavbarBootstrap.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#categories">Categories</Nav.Link>
-            <Nav.Link href="#sign-in">Sign in</Nav.Link>
-            <Nav.Link href="#register">Register</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </NavbarBootstrap.Collapse>
-      </Container>
-    </Navbar>
-  );
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>unitytrade-website</p>
+      </div>
+      <ul>
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("catagories")}}>Categories{menu==="shop"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("sign in")}}>Sign in{menu==="sign in"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("register")}}>Register{menu==="register"?<h/>:<></>}</li>
+        <li onClick={()=>{setMenu("contact")}}>Contact{menu==="contact"?<h/>:<></>}</li>
+      </ul>
+      <div className='nav-login -cart'>
+        <button>Login</button>
+        <img src={cart_icon} alt='' />
+        <div className='nav-cart-count'></div>
+      </div>
+    </div>
+  )
 }
+
+export default Navbar
