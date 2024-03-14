b4ecf9da00edf6b65b03c3018d211eb306b41b6e
Z.Sabori <zsabori82@yahoo.com>
2024-03-14 19:05:04 +0000

    resized the logo

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import "../Navbar/navbar.css";
+import { Link } from 'react-router-dom'; 
+import "./navbar.css";
 import logo from "../../assets/Images/logo.png";
 import cartIcon from "../../assets/Images/cart_icon.png";
 
@@ -10,34 +10,33 @@ const Navbar = () => {
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt="" />
-        <p>Unitytrade</p>
       </div>
       <ul>
         <li onClick={() => { setMenu("shop") }}>
           <Link style={{textDecoration:'none'}} to='/'>Shop</Link>
-          {menu === "shop" ? <></> : null}
+          {menu === "shop" ? <hr/>:<></>}
         </li>
         <li onClick={() => { setMenu("categories") }}>
           <Link style={{textDecoration:'none'}} to='/categories'>Categories</Link> 
-          {menu === "categories" ? <></> : null} 
+          {menu === "categories" ? <hr/>:<></>}
         </li>
         <li onClick={() => { setMenu("sign in") }}>
           <Link style={{textDecoration:'none'}} to='/signin'>Sign in</Link> 
-          {menu === "sign in" ? <></> : null} 
+          {menu === "sign in" ? <hr/>:<></>}
         </li>
         <li onClick={() => { setMenu("register") }}>
           <Link style={{textDecoration:'none'}} to='/register'>Register</Link> 
-          {menu === "register" ? <></> : null} 
+          {menu === "register" ? <hr/>:<></>}
         </li>
         <li onClick={() => { setMenu("contact") }}>
           <Link style={{textDecoration:'none'}} to='/contact'>Contact</Link> 
-          {menu === "contact" ? <></> : null} 
+          {menu === "contact" ? <h/>:<></>}
         </li>
       </ul>
       <div className='nav-login-cart'>
         <Link to='/login'><button>Login</button></Link>
-       <Link to='/cart'> <img src={cartIcon} alt='' /></Link>
-        <div className='nav-cart-count'></div>
+        <Link to='/cart'> <img src={cartIcon} alt='' /></Link>
+        <div className='nav-cart-count'>0</div>
       </div>
     </div>
   );
