4cf48c40ea22cd1ba22c1aa1991c9f5dce50beb3
Z.Sabori <zsabori82@yahoo.com>
2024-03-14 22:18:06 +0000

    navbar fixed and the logo size was corrected accordingly

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,63 +1,22 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "./navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from "../../assets/Images/logo.png";
-import cartIcon from "../../assets/Images/cart_icon.png";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("shop");
-  
-  const handleMenuClick = (menuItem) => {
-    setMenu(menuItem);
-  };
-
   return (
-    <div className="navbar">
+    <nav className="navbar">
       <div className="nav-logo">
-        <img src={logo} alt="" />
-      </div>
-      <ul>
-        <li onClick={() => handleMenuClick("shop")}>
-          <Link style={{ textDecoration: "none" }} to="/">
-            Shop
-          </Link>
-          {menu === "shop" && <hr />}
-        </li>
-        <li onClick={() => handleMenuClick("categories")}>
-          <Link style={{ textDecoration: "none" }} to="/categories">
-            Categories
-          </Link>
-          {menu === "categories" && <hr />}
-        </li>
-        <li onClick={() => handleMenuClick("sign in")}>
-          <Link style={{ textDecoration: "none" }} to="/signin">
-            Sign in
-          </Link>
-          {menu === "sign in" && <hr />}
-        </li>
-        <li onClick={() => handleMenuClick("register")}>
-          <Link style={{ textDecoration: "none" }} to="/register">
-            Register
-          </Link>
-          {menu === "register" && <hr />}
-        </li>
-        <li onClick={() => handleMenuClick("contact")}>
-          <Link style={{ textDecoration: "none" }} to="/contact">
-            Contact
-          </Link>
-          {menu === "contact" && <hr />}
-        </li>
-      </ul>
-      <div className="nav-login-cart">
-        <Link to="/login">
-          <button>Login</button>
+        <Link to="/shop">
+          <img src={logo} alt="Logo" />
         </Link>
-        <Link to="/cart">
-          <img src={cartIcon} alt="" />
-        </Link>
-        <div className="nav-cart-count">0</div>
       </div>
-    </div>
+      <div className="nav-actions">
+        <button><Link to="/signin">Sign In / Login</Link></button>
+        <ul>
+          <li><Link to="/contact">Contact Us</Link></li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
