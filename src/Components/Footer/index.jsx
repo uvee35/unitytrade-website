825b029769291d444ae054b221385f821361473a
Ismail Babalola <139620500+Ismail-tony@users.noreply.github.com>
2024-03-14 19:36:28 +0000

    icon change

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,10 +1,12 @@
 import './style.css'
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 function Footer() {
     return (
-        <footer className='footertext container-fluid d-flex'>
-            <div id='footerleft' className='col-6'>
+        <footer className='footertext container d-flex'>
+            <div id='footerleft' className='col-8'>
             © 2024 UnityTrade.com Ltd. |
             <a href="#"> Terms of Use </a>
             |
@@ -12,12 +14,10 @@ function Footer() {
             |
             <a href="#"> Contact Us </a> 
             </div>
-            <div id='footerright' className='text-end col-6'>
-                <a href="https://www.facebook.com" target='_blank'> Facebook </a>
-                |
-                <a href="https://www.twitter.com" target='_blank'> Twitter </a>
-                |
-                <a href="https://www.instagram.com" target='_blank'> Instagram </a>
+            <div id='footerright' className='text-end col-4'>
+                <a href="https://www.facebook.com" target='_blank'> <FontAwesomeIcon icon={faFacebook} id='icon' style={{color: '#1877F2'}}/></a>
+                <a href="https://www.twitter.com" target='_blank'> <FontAwesomeIcon icon={faTwitter} id='icon' style={{color: '#1DA1F2'}}/> </a>
+                <a href="https://www.instagram.com" target='_blank'> <FontAwesomeIcon icon={faInstagram} id='icon'  style={{color: '#E1306C'}}/> </a>
             </div>
         </footer>
     )
