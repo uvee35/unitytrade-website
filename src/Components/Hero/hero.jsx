998cc6e494aef4aa5429ebdd3423d9671eb4734d
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-15 17:55:55 +0000

    Refactor hero section component

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import heroImage from "../../assets/Images/hero.png";
 const HeroSection = () => {
-    return (
-        <div className="hero-section d-flex justify-content-center align-items-center text-center" style={{ backgroundImage: "url('src/assets/Images/Peyto Lake.jpeg')", backgroundSize: 'cover', backgroundPosition: 'center', height: '850px' }}>
-            <div className="container">
-                <h1 className='text-white display-4'>Welcome to Unity Trade</h1>
-                <p className="lead text-white">This is a sample hero section built with React.js and Bootstrap.</p>
-                <button className="btn btn-primary btn-lg">Learn More</button>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div
+      className="hero-section d-flex justify-content-center align-items-center text-center"
+      style={{
+        backgroundImage: `url(${heroImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: "500px",
+      }}
+    >
+      <div className="container">
+        <h1 className="text-white display-4">Welcome to Unity Trade</h1>
+        <p className="lead text-white">
+          This is a sample hero section built with React.js and Bootstrap.
+        </p>
+        <button className="btn btn-primary btn-lg">Learn More</button>
+      </div>
+    </div>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
