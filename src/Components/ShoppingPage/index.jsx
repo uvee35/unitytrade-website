ab57eee9457f025975d3e2a4981abe98f3328176
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-03-15 18:35:39 +0000

    added hero section

--- src/Components/ShoppingPage/index.jsx
+++ src/Components/ShoppingPage/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../Hero/hero'
 import Carousel from '../Carousel/carousel'
 
 function ShoppingPage() {
   return (
     <div>
+        <Hero />
         <Carousel />
     </div>
   )
