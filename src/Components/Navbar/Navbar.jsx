ff86402c234b88f239f0a34e20d7722ff2ea1a71
Victor Marques <151088688+uvee35@users.noreply.github.com>
2024-03-18 12:07:26 +0000

    Add authentication and user information to the navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { LoginButton } from "../Login/login";
+import { LogoutButton } from "../Login/logout";
 
 import "./navbar.css";
 import logo from "../../assets/Images/logo2.png";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
   const handleMenuClick = (menuItem) => {
     setMenu(menuItem);
   };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  // Extract username from email
+  const username =
+    isAuthenticated && user.email ? user.email.split("@")[0] : "";
 
   return (
     <div className="navbar">
@@ -18,29 +30,45 @@ const Navbar = () => {
           <img
             src={logo}
             alt="Logo"
-            style={{ maxWidth: "200%", marginLeft: "50px" }}
+            style={{
+              maxWidth: "200%",
+              marginLeft: "50px",
+            }}
             className="logo-img"
           />
         </Link>
       </div>
       <div className="nav-links">
         <ul>
-          <li onClick={() => handleMenuClick("contact")}>
-            <Link to="/contact" className={menu === "contact" ? "active" : ""}>
-              Contact Us
-            </Link>
-          </li>
-          <li onClick={() => handleMenuClick("products")}>
-            <Link to="/products" className={menu === "contact" ? "active" : ""}>
-              Products
-            </Link>
-          </li>
+          {/* Links and other navbar items */}
           <div className="nav-login-cart">
-            <Link to="/login">
-              <button>Log In/Sign Up</button>
-            </Link>
-            {/* Only display the cart icon once the user is logged in */}
-            {/* You can add logic here to conditionally render it */}
+            {isAuthenticated ? (
+              <>
+                <img
+                  src={user.picture}
+                  alt={user.name}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    borderRadius: "50%",
+                    marginRight: "10px",
+                  }}
+                />
+                {/* Display username with custom style */}
+                <span
+                  style={{
+                    marginRight: "20px",
+                    color: "white",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {username}
+                </span>
+                <LogoutButton />
+              </>
+            ) : (
+              <LoginButton />
+            )}
           </div>
         </ul>
       </div>
