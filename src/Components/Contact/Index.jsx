db6ddaf01f781bf2a577de34ad16ca7971e15c46
Tamsin Lloyd <tamsin_lloyd@hotmail.com>
2024-03-19 21:15:46 +0000

    final edits to contact us section

--- src/Components/Contact/Index.jsx
+++ src/Components/Contact/Index.jsx
@@ -13,7 +13,8 @@ return (
 <br></br>
     <div className='container'>
       <h1 className='contact-title text-start'>Contact Us:</h1>
-      <p className='contact-info'>Welcome to Unity Trade! Our dedicated customer service team is here to help you with any questions or concerns you may have. Please use the contact information below to get in touch with us.</p>
+      <p className='contact-info'>Welcome to Unity Trade! Our dedicated customer service team is here to help you with any questions or concerns you may have.</p>
+      <p className='contact-info'>Please use the contact information below to get in touch with us.</p>
       <br></br>
       <ul>
       <li>Phone: 123-456-7890</li>
